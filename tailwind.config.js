46d99a9a7a37de7a468aef9a7b8073ec9a099989
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-07-08 14:49:58 +0530

    Header and Banner add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        notosans: ["noto-sans", "Noto Sans JP, sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
